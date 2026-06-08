72a95a13fa739ff1ae7c14c01435ebad26aef788
comunidadevencedores-lgtm <comunidadevencedores@gmail.com>
2026-06-08 17:33:58 -0300

    Update page.tsx

--- src/app/agency/page.tsx
+++ src/app/agency/page.tsx
@@ -72,10 +72,10 @@ export default function AgencyPage() {
     router.push('/')
   }
 
-  function getInitials(name: string) {
-    if (!name) return '??'
-    return name.split(' ').filter(w => w.length > 0).slice(0, 2).map(w => w[0].toUpperCase()).join('')
-  }
+ function getInitials(name: string) {
+  if (!name) return '?'
+  return name.split(' ').slice(0, 2).filter(w => w.length > 0).map(w => w[0].toUpperCase()).join('')
+}
 
   function clientLink(client: Client) {
     return `${window.location.origin}/c/${agencySlug}/${client.slug}`
